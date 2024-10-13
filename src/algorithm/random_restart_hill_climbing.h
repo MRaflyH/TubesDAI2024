64e9b1e581a023b06637925a4af839ff0d36e2be
MRaflyH <raflyhendrakusuma@gmail.com>
2024-10-13 10:01:05 +0700

    Update random_restart_hill_climbing.h

--- src/algorithm/random_restart_hill_climbing.h
+++ src/algorithm/random_restart_hill_climbing.h
@@ -4,7 +4,7 @@
 #include "function/array.h"
 #include "function/magic_cube.h"
 
-magiccube random_restart_hill_climbing_value(double (*objective_function)(magiccube), int max_iteration);
-magiccube random_restart_hill_climbing_cost(double (*objective_function)(magiccube), int max_iteration);
+magiccube random_restart_hill_climbing_value(double (*value_function)(magiccube), int max_iteration);
+magiccube random_restart_hill_climbing_cost(double (*cost_function)(magiccube), int max_iteration);
 
 #endif
