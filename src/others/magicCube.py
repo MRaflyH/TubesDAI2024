7da9cfc4f1d883781c4df9c0e954a37fa8595e82
David Dewanto <david.dewanto@gmail.com>
2024-11-06 19:12:22 +0700

    finalize magicCube.py

--- src/others/magicCube.py
+++ src/others/magicCube.py
@@ -7,16 +7,19 @@ def buildMagicCube():
     return magicCube
 
 def printMagicCube(magicCube):
-    for x in range(5):
-        for y in range(5):
+    for y in range(5):
+        for z in range(5):
             print("[", end="")
-            for z in range(5):
+            for x in range(5):
                 print(magicCube[x + (y*5) + (z*25)], end="")
-                if(z != 4):
+                if(x != 4):
                     print(", ", end="")
             print("]")
-        if(x != 4):
+        if(z == 4):
             print("")   
+
+def selectorMagicCube(magicCube, x, y, z):
+    return magicCube[x + (y*5) + (z*25)]
                 
 if __name__ == "__main__":
     test = buildMagicCube()
