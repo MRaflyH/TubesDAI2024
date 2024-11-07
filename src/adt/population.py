07d0797466eff64eb5c438be99103eba9a9656c2
MRaflyH <raflyhendrakusuma@gmail.com>
2024-11-07 09:54:46 +0700

    Update population.py

--- src/adt/population.py
+++ src/adt/population.py
@@ -1,56 +1,51 @@
+from magicCube import *
+
 class Individual:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, magicCube, fitnessFunction):
+        self.magicCube = magicCube
+        self.value = fitnessFunction(magicCube)
         self.next = None
 
 class Population:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.totalValue = 0
 
-    def append(self, data):
-        new_node = Individual(data)
+    def append(self, magicCube, fitnessFunction):
+        newNode = Individual(magicCube, fitnessFunction)
         if not self.head:
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
+            self.head = newNode
+        else:
+            self.tail.next = newNode
+        self.tail = newNode
+        self.totalValue += newNode.value
 
-    # Method to insert a new node at a specific position
-    def insert(self, data, position):
-        new_node = Individual(data)
-        if position == 0:
-            new_node.next = self.head
-            self.head = new_node
-            return
-        current = self.head
-        for _ in range(position - 1):
-            if current is None:
-                raise IndexError("Position out of bounds")
-            current = current.next
-        new_node.next = current.next
-        current.next = new_node
+    def merge(self, otherPopulation):
+        if self.head and otherPopulation.head:
+            self.tail.next = otherPopulation.head
+            self.tail = otherPopulation.tail
+            self.totalValue += otherPopulation.totalValue
+        elif not self.head:
+            self.head = otherPopulation.head
+            self.tail = otherPopulation.tail
+            self.totalValue = otherPopulation.totalValue
 
-    # Method to remove a node with specific data
-    def remove(self, data):
-        current = self.head
-        if current and current.data == data:
-            self.head = current.next
-            return
-        prev = None
-        while current and current.data != data:
-            prev = current
-            current = current.next
-        if current is None:
-            return  # Data not found
-        prev.next = current.next
-
-    # Method to display the linked list
     def display(self):
         current = self.head
         while current:
-            print(current.data, end=" -> ")
+            print(current.magicCube[0], current.value, end=" -> ")
             current = current.next
         print("None")
+
+if __name__ == "__main__":
+    testPopulation = Population()
+    for i in range(5):
+        testPopulation.append(buildRandomMagicCube(), lineFunction)
+    testPopulation.display()
+    otherPopulation = Population()
+    for i in range(3):
+        otherPopulation.append(buildRandomMagicCube(), lineFunction)
+    otherPopulation.display()
+    testPopulation.merge(otherPopulation)
+    testPopulation.display()
